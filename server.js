e68cebea61d91a58530d4d5fb24c824a08c06695
Lucas Gabriel <90lucasgabriel@gmail.com>
2018-01-21 11:55:42 -0200

    Heroku

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
 // server.js
 const express = require('express');
+const path = require('path');
+const app = express();
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+
+
+
+
+/*
+// server.js
+const express = require('express');
 const http = require('http');
 const path = require('path');
 
@@ -21,3 +55,4 @@ app.set('port', port);
 const server = http.createServer(app);
 server.listen(port, () => console.log('Running'));
 //app.listen(process.env.PORT || 8080);
+*/
