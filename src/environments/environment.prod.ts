45c9f618fdd216d7769ee14fa06e9782b7177104
Lucas Gabriel <90lucasgabriel@gmail.com>
2018-01-21 21:35:27 -0200

    Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://restaurant-90lucasgabriel.herokuapp.com/public',
+  apiUrl: 'https://restaurant-90lucasgabriel.herokuapp.com/public',
   COMPANY_ID: 1
 };
